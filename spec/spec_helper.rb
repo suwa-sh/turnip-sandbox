c7d41db96232a3aa3ec2fc45364704b70dad6e45
Shinichi Suwa <suwash01@gmail.com>
2019-04-15 09:50:44 +0900

    chore(sample): chrome headless mode

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,7 @@ require 'capybara/dsl'
 require 'capybara/rspec'
 require 'turnip'
 require 'turnip/capybara'
+require 'selenium-webdriver'
 
 Capybara.default_driver = :selenium
 Capybara.ignore_hidden_elements = true
@@ -11,5 +12,8 @@ Capybara.run_server = false
 
 # Chrome
 Capybara.register_driver :selenium do |app|
-  Capybara::Selenium::Driver.new(app, browser: :chrome)
+  options = Selenium::WebDriver::Chrome::Options.new
+  options.add_argument('headless')
+  options.add_argument('--disable-gpu')
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
 end
